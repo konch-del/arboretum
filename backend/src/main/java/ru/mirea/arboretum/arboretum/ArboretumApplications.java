7f08cca57f6928d2fb08896503068b0c19c27378
Anton <antonmahov@yandex.ru>
2023-10-30 00:25:44 +0300

    Create spring-boot-applications

---
+++ backend/src/main/java/ru/mirea/arboretum/arboretum/ArboretumApplications.java
@@ -0,0 +1,11 @@
+package ru.mirea.arboretum.arboretum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArboretumApplications {
+    public static void main(String[] args) {
+        SpringApplication.run(ArboretumApplications.class, args);
+    }
+}
