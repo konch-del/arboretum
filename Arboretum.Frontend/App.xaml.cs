d96d6814bd8f62d5d7a40de5882f006590ab2c13
Влад Проничев <bluestripething@gmail.com>
2023-10-31 13:56:33 +0300

    Added frontend base on blazor

---
+++ Arboretum.Frontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Arboretum.Frontend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Arboretum.Frontend/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Arboretum.Frontend
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
